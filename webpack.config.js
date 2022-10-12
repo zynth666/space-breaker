c8eed5f5b48152497b2ecc3a407096f9ccb567a1
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2022-10-13 00:15:27 +0200

    Setup webpack and three with typescript

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        static: './dist',
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Space Breaker',
+            template: 'src/public/index.html'
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+};
