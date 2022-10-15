9a6ea9a65476aab906b8c891e19b3c755712e316
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2022-10-15 23:37:44 +0200

    Fundamentals - Primitives

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,15 @@ import "./assets/sass/styles.scss";
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-const fov = 75;
+const fov = 40;
 const aspect = 2;  // the canvas default
 const near = 0.1;
-const far = 5;
+const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 120;
 
 const scene = new THREE.Scene();
-
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-const cubes = [
-    makeInstance(geometry, 0x44aa88, 0),
-    makeInstance(geometry, 0x8844aa, -2),
-    makeInstance(geometry, 0xaa8844, 2),
-];
+scene.background = new THREE.Color(0xAAAAAA);
 
 const color = 0xFFFFFF;
 const intensity = 1;
@@ -30,19 +21,216 @@ const light = new THREE.DirectionalLight(color, intensity);
 light.position.set(-1, 2, 4);
 scene.add(light);
 
+const objects = [];
+const spread = 15;
+
 renderer.render(scene, camera);
 
 requestAnimationFrame(render);
 
-function makeInstance(geometry: THREE.BoxGeometry, color: number, x: number) {
-    const material = new THREE.MeshPhongMaterial({ color });
+function addObject(x: number, y: number, obj: THREE.Object3D) {
+    obj.position.x = x * spread;
+    obj.position.y = y * spread;
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    scene.add(obj);
+    objects.push(obj);
+}
 
-    cube.position.x = x;
+function createMaterial() {
+    const material = new THREE.MeshPhongMaterial({
+        side: THREE.DoubleSide,
+    });
 
-    return cube;
+    const hue = Math.random();
+    const saturation = 1;
+    const luminance = .5;
+    material.color.setHSL(hue, saturation, luminance);
+
+    return material;
+}
+
+function addSolidGeometry(x: number, y: number, geometry: any) {
+    const mesh = new THREE.Mesh(geometry, createMaterial());
+    addObject(x, y, mesh);
+}
+
+function addLineGeometry(x: number, y: number, geometry: any) {
+    const material = new THREE.LineBasicMaterial({ color: 0x000000 });
+    const mesh = new THREE.LineSegments(geometry, material);
+    addObject(x, y, mesh);
+}
+
+{
+    const width = 8;
+    const height = 8;
+    const depth = 8;
+    addSolidGeometry(-2, 2, new THREE.BoxGeometry(width, height, depth));
+}
+{
+    const radius = 7;
+    const segments = 24;
+    addSolidGeometry(-1, 2, new THREE.CircleGeometry(radius, segments));
+}
+{
+    const radius = 6;
+    const height = 8;
+    const segments = 16;
+    addSolidGeometry(0, 2, new THREE.ConeGeometry(radius, height, segments));
+}
+{
+    const radiusTop = 4;
+    const radiusBottom = 4;
+    const height = 8;
+    const radialSegments = 12;
+    addSolidGeometry(1, 2, new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments));
+}
+{
+    const radius = 7;
+    addSolidGeometry(2, 2, new THREE.DodecahedronGeometry(radius));
+}
+{
+    const shape = new THREE.Shape();
+    const x = -2.5;
+    const y = -5;
+    shape.moveTo(x + 2.5, y + 2.5);
+    shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+    shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+    shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+    shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+    shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+    shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+    const extrudeSettings = {
+        steps: 2,
+        depth: 2,
+        bevelEnabled: true,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelSegments: 2,
+    };
+
+    addSolidGeometry(-2, 1, new THREE.ExtrudeGeometry(shape, extrudeSettings));
+}
+{
+    const radius = 7;
+    addSolidGeometry(-1, 1, new THREE.IcosahedronGeometry(radius));
+}
+{
+    const points = [];
+    for (let i = 0; i < 10; ++i) {
+        points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
+    }
+    addSolidGeometry(0, 1, new THREE.LatheGeometry(points));
+}
+{
+    const radius = 7;
+    addSolidGeometry(1, 1, new THREE.OctahedronGeometry(radius));
+}
+{
+    const width = 9;
+    const height = 9;
+    const widthSegments = 2;
+    const heightSegments = 2;
+    addSolidGeometry(-2, 0, new THREE.PlaneGeometry(width, height, widthSegments, heightSegments));
+}
+{
+    const verticesOfCube = [
+        -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
+        -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
+    ];
+    const indicesOfFaces = [
+        2, 1, 0, 0, 3, 2,
+        0, 4, 7, 7, 3, 0,
+        0, 1, 5, 5, 4, 0,
+        1, 2, 6, 6, 5, 1,
+        2, 3, 7, 7, 6, 2,
+        4, 5, 6, 6, 7, 4,
+    ];
+    const radius = 7;
+    const detail = 2;
+    addSolidGeometry(-1, 0, new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, radius, detail));
+}
+{
+    const innerRadius = 2;
+    const outerRadius = 7;
+    const segments = 18;
+    addSolidGeometry(0, 0, new THREE.RingGeometry(innerRadius, outerRadius, segments));
+}
+{
+    const shape = new THREE.Shape();
+    const x = -2.5;
+    const y = -5;
+    shape.moveTo(x + 2.5, y + 2.5);
+    shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+    shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+    shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+    shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+    shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+    shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+    addSolidGeometry(1, 0, new THREE.ShapeGeometry(shape));
+}
+{
+    const radius = 7;
+    const widthSegments = 12;
+    const heightSegments = 8;
+    addSolidGeometry(2, 0, new THREE.SphereGeometry(radius, widthSegments, heightSegments));
+}
+{
+    const radius = 7;
+    addSolidGeometry(-2, -1, new THREE.TetrahedronGeometry(radius));
+}
+{
+    const radius = 5;
+    const tubeRadius = 2;
+    const radialSegments = 8;
+    const tubularSegments = 24;
+    addSolidGeometry(0, -1, new THREE.TorusGeometry(radius, tubeRadius, radialSegments, tubularSegments));
+}
+{
+    const radius = 3.5;
+    const tube = 1.5;
+    const radialSegments = 8;
+    const tubularSegments = 64;
+    const p = 2;
+    const q = 3;
+    addSolidGeometry(1, -1, new THREE.TorusKnotGeometry(radius, tube, tubularSegments, radialSegments, p, q));
+}
+{
+    class CustomSinCurve extends THREE.Curve<THREE.Vector3> {
+        scale: number;
+        constructor(scale: number) {
+            super();
+            this.scale = scale;
+        }
+        getPoint(t: number) {
+            const tx = t * 3 - 1.5;
+            const ty = Math.sin(2 * Math.PI * t);
+            const tz = 0;
+            return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+        }
+    }
+
+    const path = new CustomSinCurve(4);
+    const tubularSegments = 20;
+    const radius = 1;
+    const radialSegments = 8;
+    const closed = false;
+    addSolidGeometry(2, -1, new THREE.TubeGeometry(path, tubularSegments, radius, radialSegments, closed));
+}
+{
+    const width = 8;
+    const height = 8;
+    const depth = 8;
+    const thresholdAngle = 15;
+    addLineGeometry(-1, -2, new THREE.EdgesGeometry(
+        new THREE.BoxGeometry(width, height, depth),
+        thresholdAngle));
+}
+{
+    const width = 8;
+    const height = 8;
+    const depth = 8;
+    addLineGeometry(1, -2, new THREE.WireframeGeometry(new THREE.BoxGeometry(width, height, depth)));
 }
 
 function render(time: number) {
@@ -53,14 +241,6 @@ function render(time: number) {
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
     }
-
-    cubes.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
-        const rot = time * speed;
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-    });
-
 
     renderer.render(scene, camera);
 
