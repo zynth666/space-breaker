8fa9d9f1b8a1b5a0dd15fd08e71e1ab7b9c13e55
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2022-10-18 21:44:01 +0200

    Fundamentals - Textures

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,43 @@
 import * as THREE from "three";
-import GUI from 'lil-gui';
 import "./assets/sass/styles.scss";
 
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-const fov = 40;
+const fov = 75;
 const aspect = 2;  // the canvas default
 const near = 0.1;
-const far = 1000;
+const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 50, 0);
-camera.up.set(0, 0, 1);
-camera.lookAt(0, 0, 0);
+camera.position.z = 2;
 
 const scene = new THREE.Scene();
 
-{
-    const color = 0xFFFFFF;
-    const intensity = 3;
-    const light = new THREE.PointLight(color, intensity);
-    scene.add(light);
-}
+/* const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.PointLight(color, intensity);
+scene.add(light); */
 
-// an array of objects whose rotation to update
-const objects: THREE.Object3D[] = [];
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-// use just one sphere for everything
-const radius = 1;
-const widthSegments = 6;
-const heightSegments = 6;
-const sphereGeometry = new THREE.SphereGeometry(
-    radius, widthSegments, heightSegments);
+const cubes: any = [];
+const loader = new THREE.TextureLoader();
 
-const solarSystem = new THREE.Object3D();
-scene.add(solarSystem);
-objects.push(solarSystem);
+const materials = [
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-1.jpg')) }),
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-2.jpg')) }),
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-3.jpg')) }),
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-4.jpg')) }),
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-5.jpg')) }),
+    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-6.jpg')) }),
+];
 
-const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
-const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-sunMesh.scale.set(5, 5, 5);  // make the sun large
-solarSystem.add(sunMesh);
-objects.push(sunMesh);
-
-const earthOrbit = new THREE.Object3D();
-earthOrbit.position.x = 10;
-solarSystem.add(earthOrbit);
-objects.push(earthOrbit);
-
-const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
-const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-earthOrbit.add(earthMesh);
-objects.push(earthMesh);
-
-const moonOrbit = new THREE.Object3D();
-moonOrbit.position.x = 2;
-earthOrbit.add(moonOrbit);
-
-const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 });
-const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-moonMesh.scale.set(.5, .5, .5);
-moonOrbit.add(moonMesh);
-objects.push(moonMesh);
-
-class AxisGridHelper {
-    grid: THREE.GridHelper;
-    axes: THREE.AxesHelper;
-    _visible: any;
-    constructor(node: any, units = 10) {
-        const axes = new THREE.AxesHelper();
-        (axes.material as THREE.Material).depthTest = false;
-        axes.renderOrder = 2;  // after the grid
-        node.add(axes);
-
-        const grid = new THREE.GridHelper(units, units);
-        (grid.material as THREE.Material).depthTest = false;
-        grid.renderOrder = 1;
-        node.add(grid);
-
-        this.grid = grid;
-        this.axes = axes;
-        this.visible = false;
-    }
-    get visible() {
-        return this._visible;
-    }
-    set visible(v) {
-        this._visible = v;
-        this.grid.visible = v;
-        this.axes.visible = v;
-    }
-}
-
-const gui = new GUI();
-function makeAxisGrid(node: any, label: any, units: any) {
-    const helper = new AxisGridHelper(node, units);
-    gui.add(helper, 'visible').name(label);
-}
-
-makeAxisGrid(solarSystem, 'solarSystem', 25);
-makeAxisGrid(sunMesh, 'sunMesh', 10);
-makeAxisGrid(earthOrbit, 'earthOrbit', 10);
-makeAxisGrid(earthMesh, 'earthMesh', 10);
-makeAxisGrid(moonOrbit, 'moonOrbit', 10);
-makeAxisGrid(moonMesh, 'moonMesh', 10);
+const cube = new THREE.Mesh(geometry, materials);
+scene.add(cube);
+cubes.push(cube);
 
 renderer.render(scene, camera);
 
@@ -118,8 +52,11 @@ function render(time: number) {
         camera.updateProjectionMatrix();
     }
 
-    objects.forEach((obj) => {
-        obj.rotation.y = time;
+    cubes.forEach((cube: any, ndx: any) => {
+        const speed = .2 + ndx * .1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;
     });
 
     renderer.render(scene, camera);
