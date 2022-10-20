4752eacbb1fec638e90963427ccd4f96a7a14f88
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2022-10-20 22:31:04 +0200

    Fundamentals - Lights

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,131 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./assets/sass/styles.scss";
+import GUI from "lil-gui";
 
-const canvas = document.querySelector('#canvas');
+const canvas: HTMLElement = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-const fov = 75;
+const fov = 45;
 const aspect = 2;  // the canvas default
 const near = 0.1;
-const far = 5;
+const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.set(0, 10, 20);
 
 const scene = new THREE.Scene();
 
-/* const color = 0xFFFFFF;
-const intensity = 3;
-const light = new THREE.PointLight(color, intensity);
-scene.add(light); */
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 5, 0);
+controls.update();
 
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const planeSize = 40;
 
-const cubes: any = [];
 const loader = new THREE.TextureLoader();
+const texture = loader.load(require('./assets/images/checker.png'));
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+const repeats = planeSize / 2;
+texture.repeat.set(repeats, repeats);
 
-const materials = [
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-1.jpg')) }),
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-2.jpg')) }),
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-3.jpg')) }),
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-4.jpg')) }),
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-5.jpg')) }),
-    new THREE.MeshBasicMaterial({ map: loader.load(require('./assets/images/flower-6.jpg')) }),
-];
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+const planeMat = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+});
+const mesh = new THREE.Mesh(planeGeo, planeMat);
+mesh.rotation.x = Math.PI * -.5;
+scene.add(mesh);
 
-const cube = new THREE.Mesh(geometry, materials);
-scene.add(cube);
-cubes.push(cube);
+{
+    const cubeSize = 4;
+    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' });
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+    mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+    scene.add(mesh);
+}
+{
+    const sphereRadius = 3;
+    const sphereWidthDivisions = 32;
+    const sphereHeightDivisions = 16;
+    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+    const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' });
+    const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+    scene.add(mesh);
+}
+
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.SpotLight(color, intensity);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
+scene.add(light);
+scene.add(light.target);
+
+const helper = new THREE.SpotLightHelper(light);
+scene.add(helper);
+
+class ColorGUIHelper {
+    object: any;
+    prop: any;
+    constructor(object: any, prop: any) {
+        this.object = object;
+        this.prop = prop;
+    }
+    get value() {
+        return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+        this.object[this.prop].set(hexString);
+    }
+}
+
+class DegRadHelper {
+    obj: any;
+    prop: any;
+    constructor(obj: any, prop: any) {
+        this.obj = obj;
+        this.prop = prop;
+    }
+    get value() {
+        return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+    }
+    set value(v) {
+        this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+    }
+}
+
+const gui = new GUI();
+gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+gui.add(light, 'intensity', 0, 2, 0.01);
+gui.add(light, 'distance', 0, 40).onChange(updateLight);
+gui.add(new DegRadHelper(light, 'angle'), 'value', 0, 90).name('angle').onChange(updateLight);
+gui.add(light, 'penumbra', 0, 1, 0.01);
+
+
+makeXYZGUI(gui, light.position, 'position', updateLight);
+makeXYZGUI(gui, light.target.position, 'target', updateLight);
 
 renderer.render(scene, camera);
 
 requestAnimationFrame(render);
+
+function updateLight() {
+    light.target.updateMatrixWorld();
+    helper.update();
+}
+updateLight();
+
+function makeXYZGUI(gui: { addFolder: (arg0: any) => any; }, vector3: any, name: any, onChangeFn: any) {
+    const folder = gui.addFolder(name);
+    folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+    folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+    folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+    folder.open();
+}
 
 function render(time: number) {
     time *= 0.001;
@@ -52,12 +136,6 @@ function render(time: number) {
         camera.updateProjectionMatrix();
     }
 
-    cubes.forEach((cube: any, ndx: any) => {
-        const speed = .2 + ndx * .1;
-        const rot = time * speed;
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-    });
 
     renderer.render(scene, camera);
 
