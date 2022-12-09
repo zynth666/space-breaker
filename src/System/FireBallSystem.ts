eb96e1193a1e4cd1d47d5eecf2c9e67be92964b4
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2022-12-09 21:07:18 +0100

    Attach ball to root scene on firing

--- src/System/FireBallSystem.ts
+++ src/System/FireBallSystem.ts
@@ -1,4 +1,5 @@
 import Fireable from "../component/Fireable";
+import Mesh from "../component/Mesh";
 import Velocity from "../component/Velocity";
 import { Entity } from "../entity/types";
 import KeyboardControls from "./KeyboardControls";
@@ -11,6 +12,7 @@ export default class FireBallSystem extends System {
         entities.forEach(entity => {
             const entityContainer = this.engine.getComponents(entity);
             const fireable = entityContainer.get(Fireable);
+            const mesh = entityContainer.get(Mesh);
 
             if (fireable.value && KeyboardControls.isPressed(fireable.fireKey)) {
                 fireable.value = false;
@@ -18,6 +20,9 @@ export default class FireBallSystem extends System {
                 const velocity = new Velocity();
                 velocity.vec.set(Math.random() * 2 - 1, 0, -1);
                 this.engine.addComponent(entity, velocity);
+
+                const scene = mesh.three.parent.parent;
+                scene.attach(mesh.three);
             }
         });
     }
